d00b40470ffba7ad5b3767b339df1872ecde3f9d
gahag <gabriel.s.b@live.com>
2018-01-03 11:40:24 -0200

    Initial commit.

---
+++ homepage.js
@@ -0,0 +1,7 @@
+function email() {
+    var a = "bastos.gabriel";
+    var b = "dcc.ufmg.br";
+    var c = "@";
+    var d = a + c + b;
+    document.write("<a href=" + "mail" + "to:" + d + ">" + d + "</a>");
+}
